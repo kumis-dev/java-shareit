1fc19d59428eaa98b0672be256b2f6fe29f2f892
kumis <dxdkymis@mail.ru>
2026-02-17 19:09:12 +0300

    fix: remove comments and todo

--- server/src/main/java/ru/practicum/shareit/booking/Booking.java
+++ server/src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -7,9 +7,6 @@ import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
-/**
- * TODO Sprint add-bookings.
- */
 @Data
 @Entity
 @Table(name = "bookings")
--- server/src/main/java/ru/practicum/shareit/item/model/Item.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -5,9 +5,6 @@ import lombok.Data;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
-/**
- * TODO Sprint add-controllers.
- */
 @Data
 @Entity
 @Table(name = "items")
